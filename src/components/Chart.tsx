333d99c881c4cd49313d173623cad2b249e0e669
Ashish Dikonda <85207377+dikondaashish@users.noreply.github.com>
2026-04-24 16:57:19 -0400

    feat: increase bar chart size with responsive container

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,14 +1,18 @@
 "use client";
 
-import { BarChart, Bar, XAxis, YAxis, Tooltip } from "recharts";
+import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer } from "recharts";
 
 export default function Chart({ data }: any) {
   return (
-    <BarChart width={600} height={300} data={data}>
-      <XAxis dataKey="date" />
-      <YAxis />
-      <Tooltip />
-      <Bar dataKey="water" />
-    </BarChart>
+    <div className="w-full h-105">
+      <ResponsiveContainer width="100%" height="100%">
+        <BarChart data={data} margin={{ top: 16, right: 24, left: 8, bottom: 8 }}>
+          <XAxis dataKey="date" />
+          <YAxis />
+          <Tooltip />
+          <Bar dataKey="water" />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
   );
 }
